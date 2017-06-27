576f72d2cadc72f640a42fb40777835263576593
frank <vnussolution@gmail.com>
2017-06-26 22:37:42 -0700

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -16,6 +16,15 @@ app.get('/', (req, res) => {
     res.send('yessss');
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
 app.post('/todos', (req, res) => {
     var newTodo = new Todo({ text: req.body.text });
 
