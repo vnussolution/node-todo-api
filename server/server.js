91c88edd2e0775455ded1b38ab6446b05e3632cb
frank <vnussolution@gmail.com>
2017-06-29 15:47:20 -0700

    add patch /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 
 var { mongoose } = require('./db/mongoose');
@@ -87,7 +88,45 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(400).send('error ', e);
     });
-})
+});
+
+app.patch(`/todos/:id`, (req, res) => {
+    var id = req.params.id;
+    console.log(id);
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) return res.status(404).send('id is invalid');
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    // Todo.findOneAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    //     if (!todo) return res.status(404).send('no todo found');
+
+    //     res.send({ todo });
+    // }).catch((e) => res.status(400).send(e));
+
+    Todo.findById(id, (err, todo) => {
+        if (err) return res.status(400).send(e);
+
+        if (!todo) return res.status(404).send('no todo found');
+
+        todo.text = body.text;
+        todo.save((error, result) => {
+            if (error) console.log('eee', error);
+
+            res.send({ result });
+
+        });
+    })
+
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`started on port ${PORT}`);
